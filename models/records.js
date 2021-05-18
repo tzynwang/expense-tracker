06103bed857961c98a157773cb6352cf81baedf4
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-05-18 20:42:34 +0800

    refactor: modify record's schema

--- models/records.js
+++ models/records.js
@@ -12,7 +12,7 @@ const recordSchema = new Schema({
     required: true
   },
   date: {
-    type: Date,
+    type: String,
     required: true
   },
   amount: {
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,5 +1,4 @@
 const Records = require('../records')
-const Categories = require('../categories')
 const db = require('../../config/mongoose')
 
 const sampleRecords = require('../../sampleRecords.json')
@@ -7,18 +6,12 @@ const sampleRecords = require('../../sampleRecords.json')
 db.once('open', () => {
   console.log('mongodb connect')
   sampleRecords.results.forEach(eachRecord => {
-    // get category id-name pair
-    Categories.find({ name: eachRecord.category }, { _id: 1 })
-      .lean()
-      .then(result => {
-        const categoryId = result[0]._id
-        Records.create({
-          name: eachRecord.name,
-          category: categoryId,
-          date: new Date().toISOString(), // the time that runs seeds scripts
-          amount: eachRecord.amount
-        })
-      })
+    Records.create({
+      name: eachRecord.name,
+      category: eachRecord.category,
+      date: new Date().toISOString().slice(0, 10), // the date that runs seeds scripts
+      amount: eachRecord.amount
+    })
   })
   console.log('Generate records by seeder DONE')
 })
