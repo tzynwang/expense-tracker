e1fbda8d8af65d8bd03c219a475417c45a6035be
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-06-23 19:16:19 +0800

    style: add line break for mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const db = mongoose.connection
 
