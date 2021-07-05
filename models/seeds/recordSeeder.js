14a236ca9005cde7b5e0c21cae8c74870e5e42bd
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-07-05 22:50:47 +0800

    refact: update schema to fit auth feature

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,18 +1,25 @@
-const Records = require('../records')
+const Record = require('../records')
+const User = require('../users')
 const db = require('../../config/mongoose')
-
+const sampleUsers = require('./sampleUsers')
 const sampleRecords = require('./sampleRecords.json')
 
 db.once('open', async () => {
-  console.log('mongodb connect records')
-  for (const record of sampleRecords) {
-    await Records.create({
-      name: record.name,
-      category: record.category,
-      date: new Date().toISOString().slice(0, 10), // the date that runs seeds scripts
-      amount: record.amount
-    })
+  console.log('mongodb connect from recordSeeder')
+
+  for (const user of sampleUsers) {
+    const targetUser = await User.findOne({ email: user.email }).lean()
+    for (const record of sampleRecords) {
+      await Record.create({
+        name: record.name,
+        category: record.category,
+        date: new Date().toISOString().slice(0, 10), // the date that runs seeds scripts
+        amount: record.amount,
+        userId: targetUser._id
+      })
+    }
   }
-  console.log('Generate records by seeder DONE')
+
+  console.log('mongodb recordSeeder done')
   db.close()
 })
