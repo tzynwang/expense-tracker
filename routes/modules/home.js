4edde4aa3f1e82d5d3376d144881294a01bdfb99
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-07-05 22:52:19 +0800

    refact: pull data by userId, add welcome endpoint

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,20 +4,22 @@ const router = express.Router()
 const Record = require('../../models/records')
 const Category = require('../../models/categories')
 
-router.get('/', async (req, res) => {
-  const records = await Record.find().sort({ date: 'desc' }).lean()
+const { hasLoggedIn, hasLoggedOut } = require('../../auth/auth')
+
+router.get('/', hasLoggedIn, async (req, res) => {
+  const records = await Record.find({ userId: req.user._id, isDelete: false }).sort({ date: 'desc' }).lean()
   let totalAmount = 0
   records.forEach(record => (totalAmount += record.amount))
   const categories = await Category.find().lean()
   res.render('index', { records, categories, totalAmount })
 })
 
-router.post('/filter', async (req, res) => {
+router.post('/filter', hasLoggedIn, async (req, res) => {
   const { category } = req.body
   const results = await Record.aggregate([
     {
-      // similar to find({}, { category })
-      $match: { category }
+      // similar to find({ category })
+      $match: { category, userId: req.user._id }
     },
     {
       // join with collection 'categories'
@@ -30,6 +32,10 @@ router.post('/filter', async (req, res) => {
     }
   ])
   res.send(results)
+})
+
+router.get('/welcome', hasLoggedOut, (req, res) => {
+  res.render('welcome', { notNavBar: true })
 })
 
 module.exports = router
