2fc2c995d6dae0aa753a46cd87dd3062f8defa5b
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-07-06 11:54:34 +0800

    feat: add user-greeting message

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,7 +11,8 @@ router.get('/', hasLoggedIn, async (req, res) => {
   let totalAmount = 0
   records.forEach(record => (totalAmount += record.amount))
   const categories = await Category.find().lean()
-  res.render('index', { records, categories, totalAmount, indexScripts: true })
+  const username = req.user.username
+  res.render('index', { username, records, categories, totalAmount, indexScripts: true })
 })
 
 router.post('/', hasLoggedIn, async (req, res) => {
