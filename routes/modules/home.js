dab5818ddae523ac8a9056d09b6e3ea126f61eae
tzyn (Windows Fork) <tzyn.wang@gmail.com>
2021-07-06 21:24:06 +0800

    feat: load current month expense by default

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,12 +7,19 @@ const Category = require('../../models/categories')
 const { hasLoggedIn, hasLoggedOut } = require('../../auth/auth')
 
 router.get('/', hasLoggedIn, async (req, res) => {
-  const records = await Record.find({ userId: req.user._id, isDelete: false }).sort({ date: 'desc' }).lean()
+  const currentMonth = new Date().toISOString().slice(0, 7) // YYYY-MM
+  const records = await Record.find({
+    userId: req.user._id,
+    date: { $regex: currentMonth },
+    isDelete: false
+  })
+    .sort({ date: 'desc' })
+    .lean()
   let totalAmount = 0
   records.forEach(record => (totalAmount += record.amount))
   const categories = await Category.find().lean()
   const username = req.user.username
-  res.render('index', { username, records, categories, totalAmount, indexScripts: true })
+  res.render('index', { username, records, categories, totalAmount, currentMonth, indexScripts: true })
 })
 
 router.post('/', hasLoggedIn, async (req, res) => {
